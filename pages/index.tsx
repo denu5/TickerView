65761ac98007342f6f762431ac9ee83109b1fe7b
denu5 <denus.robert@gmail.com>
2022-02-14 11:27:03 +0100

    fix build

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { GetServerSideProps, NextPage } from 'next';
 import getConfig from 'next/config'
 import NextLink from 'next/link';
 
-import { Page, Text, Card, Badge, Spacer, ButtonGroup, Button } from '@geist-ui/core';
+import { Page, Text, Card, Badge, Spacer, ButtonGroup, Button, Divider } from '@geist-ui/core';
 import { ITickers, ITickersQuery, referenceClient, } from '@polygon.io/client-js';
 
 const { publicRuntimeConfig } = getConfig();
@@ -27,18 +27,20 @@ const Home: NextPage<ITickers> = ({ results }) => {
     <Page>
       <Text h1>Tickers</Text>
 
-      <ButtonGroup my={2}>
+      <ButtonGroup mb={2}>
         {Object.keys(MarketType).map((type) => <NextLink key={type} href={`/?market=${type}`} passHref><a><Button >{type}</Button></a></NextLink>)}
       </ButtonGroup>
 
-      {results.map(({ ticker, name, market, composite_figi, type }) => (
-        <NextLink key={ticker} href={`/ticker/${ticker}`} passHref>
+      <Divider mb={2} /> 
+
+      {results.map(({ ticker, name, market, composite_figi, type }, k) => (
+        <NextLink key={k + ticker} href={`/ticker/${ticker}`} passHref>
           <a>
             <Card shadow mb={2} width="100%">
               <Text h4 my={0}>{ticker}</Text>
               <Text my={0}>{name}</Text>
               <Card.Footer>
-                {[market, type, composite_figi].map(feature => (feature && <Badge type="secondary" mr={1}>{feature.toUpperCase()}</Badge>))}
+                {[market, type, composite_figi].map((feature,k) => (feature && <Badge key={k} type="secondary" mr={1}>{feature.toUpperCase()}</Badge>))}
               </Card.Footer>
             </Card>
           </a>
